ab4d850a224bb20a0968d5bb29d1a38823056e05
kmok <kmok@thoughtworks.com>
2020-07-21 15:17:54 +0200

    km: removed default

--- sbin/buildAndRunLocal.sh
+++ sbin/buildAndRunLocal.sh
@@ -4,9 +4,10 @@ set -ex
 SCRIPT_DIR=$(cd "$(dirname "$0")" ; pwd -P)
 ROOT_DIR="${SCRIPT_DIR}/.."
 
-java_8_home=${1:-/Library/Java/JavaVirtualMachines/jdk1.8.0_172.jdk/Contents/Home}
+java_8_home=$1
 if [ -z "${java_8_home}" ]; then
   echo "java_8_home not set. Usage <JAVA 8 HOME PATH>"
+  echo "Hint: run `java -version`. If not running Java 8, switch to Java 8. Your Java 8 Home path can be found by running `echo $JAVA_HOME`"
   exit 1
 fi
 
@@ -28,6 +29,6 @@ pushd "${ROOT_DIR}/StationTransformerNYC" > /dev/null
   sbt package
 popd > /dev/null
 
-#pushd "${ROOT_DIR}/docker" > /dev/null
-#  docker-compose.sh --project-directory . -f docker-compose.yml up --build -d
-#popd > /dev/null
+pushd "${ROOT_DIR}/docker" > /dev/null
+  docker-compose.sh --project-directory . -f docker-compose.yml up --build -d
+popd > /dev/null
